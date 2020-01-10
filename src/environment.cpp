b7bc87dd07178f126e8383ecf96617a0a59d00e1
waterwheel31 <waterwheel31@gmail.com>
2020-01-10 16:02:19 +0400

    section 2

--- src/environment.cpp
+++ src/environment.cpp
@@ -51,7 +51,11 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     //renderRays(viewer, lidar->position, inputCloud);
     renderPointCloud(viewer, inputCloud, "inputCloud");
     // TODO:: Create point processor
-  
+    ProcessPointClouds<pcl::PointXYZ> pointProcessor; 
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer,segmentCloud.first,"obstCloud",Color(1,0,0));
+    renderPointCloud(viewer,segmentCloud.second,"planeCloud",Color(0,1,0));
+
 }
 
 
